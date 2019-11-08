8fd9441612583447b41fbd420935cf4f52925032
nicolas le flohic <vikking@mbp-de-nicolas.lan>
2019-11-08 13:20:58 +0100

    Add task

--- src/App.js
+++ src/App.js
@@ -3,24 +3,39 @@ import './App.css';
 
 function App() {
   const [tasks, setTasks] = useState([]);
+  const [taskInput, setTaskInput] = useState("");
+  const [placeHolder, setPlaceHolder] = useState("");
+
 
   return (
     <div className="App">
       <h1>Todo List</h1>
       <div className="tasklist">
-        <div className="task">
-          <span>X</span><h3>Faire les courses</h3>
-        </div>
-        <div className="task">
-          <span>X</span><h3>Sortir le chien</h3>
-        </div>
+        {
+          tasks.map((task, id) => {
+            return (
+              <div key={id} className="task">
+                <span>X</span> <h3>{task.name}</h3>
+              </div>);
+          })}
       </div>
-
-      <form>
-        <input className="input" placeholder="Tache"></input>
+      <form className="form" onSubmit={(event) => {
+        event.preventDefault();
+        const newTasks = [...tasks];
+        newTasks.push({ name: taskInput });
+        setTasks(newTasks);
+        console.log("newtasks " + newTasks);
+      }}>
+        <input className="input" type="text" placeholder="tache" value={placeHolder} onChange={event => {
+          setTaskInput(event.target.value);
+          setPlaceHolder(event.target.value);
+        }} />
+        <input className="button" type="submit" value="Ajouter une tache" onClick={event => {
+          setPlaceHolder("");
+        }
+        } />
       </form>
-      <button className="button">Ajouter une tache</button>
-    </div>
+    </div >
   );
 }
 
