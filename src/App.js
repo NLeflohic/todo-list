d90db0fb052593e0be4d3faa4a032318ab92102b
nicolas le flohic <vikking@mbp-de-nicolas.lan>
2019-11-08 14:59:36 +0100

    Add task, delete task, lined task

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import './App.css';
 
 function App() {
   const [tasks, setTasks] = useState([]);
+  const [style, setStyle] = useState({});
   const [taskInput, setTaskInput] = useState("");
   const [placeHolder, setPlaceHolder] = useState("");
-
 
   return (
     <div className="App">
@@ -15,16 +15,41 @@ function App() {
           tasks.map((task, id) => {
             return (
               <div key={id} className="task">
-                <span>X</span> <h3>{task.name}</h3>
+                <span onClick={
+                  (event) => {
+                    event.preventDefault();
+                    const newTasks = [...tasks];
+                    for (let i = 0; i < newTasks.length; i++) {
+                      if (newTasks[i].name === task.name) {
+                        newTasks.splice(i, 1);
+                      }
+                    };
+                    setTasks(newTasks);
+                  }
+                }>X</span> <h3 style={task.style} onClick={
+                  (event) => {
+                    task.style = { "textDecoration": "line-through" };
+                    const newTasks = [...tasks];
+                    const tabOrdered = [];
+                    for (let i = 0; i < newTasks.length; i++) {
+                      if (newTasks[i].style.textDecoration === "none") {
+                        tabOrdered.unshift(newTasks[i]);
+                      } else {
+                        tabOrdered.push(newTasks[i]);
+                      }
+                    }
+                    setTasks(tabOrdered);
+                    setStyle({ "textDecoration": "line-through" });
+                  }
+                }>{task.name}</h3>
               </div>);
           })}
       </div>
       <form className="form" onSubmit={(event) => {
         event.preventDefault();
         const newTasks = [...tasks];
-        newTasks.push({ name: taskInput });
+        newTasks.push({ name: taskInput, style: { "textDecoration": "none" } });
         setTasks(newTasks);
-        console.log("newtasks " + newTasks);
       }}>
         <input className="input" type="text" placeholder="tache" value={placeHolder} onChange={event => {
           setTaskInput(event.target.value);
